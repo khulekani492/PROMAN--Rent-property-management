7c76f3c2d44cff78cb43909b3a083dc8d5d6e708
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-08-30 15:25:23 +0200

    adjusted the tests to use junit5 imports

--- Mastende/src/test/java/backend/database/DatabaseTest.java
+++ Mastende/src/test/java/backend/database/DatabaseTest.java
@@ -1,7 +1,50 @@
 package backend.database;
 
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.sql.*;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class DatabaseTest {
+public class DatabaseTest {
+    private Database dummyconnection;
+    private static final String D_URL = "jdbc:sqlite:rentalRooms.db";
+
+//    @AfterEach
+//    public  void  droptable()throws  SQLException{
+//        String droptable = """
+//                DROP table tenants;
+//                """;
+//        try (Connection conn = DriverManager.getConnection(D_URL)){
+//            Statement exct = conn.createStatement();
+//            exct.execute(droptable);
+//
+//        }
+//    }
+
+    @Test
+     public void testNumberOfRooms() throws SQLException {
+        dummyconnection = new Database(D_URL);
+        String sql = "SELECT COUNT(*) AS total FROM tenants";
+        try (Connection conn = DriverManager.getConnection(D_URL);
+             Statement ptsm = conn.createStatement()){
+             ResultSet database_rows = ptsm.executeQuery(sql);
+             dummyconnection.numberofRooms(8);
+
+            int total_rows = 0;
+
+            if (database_rows.next()) {
+                total_rows = database_rows.getInt("total");  // get the actual count
+            }
+            assertEquals(5,total_rows);
+
+        }
+
+    }
 
 }
