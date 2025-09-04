80d0b78b80a6741c854030c4c9509ebc3b36794a
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-09-04 20:49:48 +0200

    added more input parameters for roomStatus

--- Mastende/src/main/java/backend/database/Property.java
+++ Mastende/src/main/java/backend/database/Property.java
@@ -3,8 +3,9 @@ package backend.database;
 import java.sql.SQLException;
 
 public interface Property {
-    void numberofRooms(int room_numbers) throws SQLException;
-    void roomStatus(String name, int room_no, String move_in_date, String employment_status);
+//    void numberofRooms(int room_numbers) throws SQLException;
+    void roomStatus(String name, int room_no, String move_in_date, String employment_status, String cellphone,String pay_day);
+
 
 }
 
