bfa28ff8cb5b6174640e0897136badbc0db779fa
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-09-05 17:00:07 +0200

    a abstract method addPropertyInfo

--- Mastende/src/main/java/backend/database/Property.java
+++ Mastende/src/main/java/backend/database/Property.java
@@ -3,7 +3,8 @@ package backend.database;
 import java.sql.SQLException;
 
 public interface Property {
-//    void numberofRooms(int room_numbers) throws SQLException;
+    //void numberofRooms(int room_numbers) throws SQLException;
+    void addProperty_info(String property_name,String number_of_rooms, String rent, String address,String contact);
     void roomStatus(String name, int room_no, String move_in_date, String employment_status, String cellphone,String pay_day);
 
 
