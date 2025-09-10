d1b9a27591caec42d6c7115e3b2c0cfbf18238bb
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-09-10 21:45:50 +0200

    abstract data type updated

--- Mastende/src/main/java/backend/database/Property.java
+++ Mastende/src/main/java/backend/database/Property.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public interface Property {
     //void numberofRooms(int room_numbers) throws SQLException;
-    void addProperty_info(String property_name,String number_of_rooms, String rent, String address,String contact);
+    void addProperty_info(String property_name,int number_of_rooms, int rent, String address,String contact);
     void roomStatus(String name, int room_no, String move_in_date, String employment_status, String cellphone,String pay_day);
 
 
