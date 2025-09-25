98f02e3f7bdf7683a6025e487c28b7ecd1e9f1d2
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-09-25 13:10:06 +0200

    Seperation of concern, SessionUtil class for file-based sessions

--- Mastende/src/main/java/API/SessionUtil.java
+++ Mastende/src/main/java/API/SessionUtil.java
@@ -1,4 +1,31 @@
 package API;
+import java.io.File;
+import org.eclipse.jetty.http.HttpCookie;
+import org.eclipse.jetty.server.session.DatabaseAdaptor;
+import org.eclipse.jetty.server.session.DefaultSessionCache;
+import org.eclipse.jetty.server.session.FileSessionDataStore;
+import org.eclipse.jetty.server.session.JDBCSessionDataStoreFactory;
+import org.eclipse.jetty.server.session.SessionCache;
+import org.eclipse.jetty.server.session.SessionHandler;
 
 public class SessionUtil {
+    // --------------------- SQL Sessions Handler ---------------------
+    public static SessionHandler fileSessionHandler() {
+        SessionHandler sessionHandler = new SessionHandler();
+        SessionCache sessionCache = new DefaultSessionCache(sessionHandler);
+        sessionCache.setSessionDataStore(fileSessionDataStore());
+        sessionHandler.setSessionCache(sessionCache);
+        sessionHandler.setHttpOnly(true);
+        // make additional changes to your SessionHandler here
+        return sessionHandler;
+    }
+
+    private static FileSessionDataStore fileSessionDataStore() {
+        FileSessionDataStore fileSessionDataStore = new FileSessionDataStore();
+        File baseDir = new File(System.getProperty("java.io.tmpdir"));
+        File storeDir = new File(baseDir, "javalin-session-store");
+        storeDir.mkdir();
+        fileSessionDataStore.setStoreDir(storeDir);
+        return fileSessionDataStore;
+    }
 }
