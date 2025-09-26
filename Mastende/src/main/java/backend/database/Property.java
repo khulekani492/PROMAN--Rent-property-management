1478b32c8f824a20556ffa84dd0795caad02f82d
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-09-26 23:10:42 +0200

    added int debt to property abstract methods

--- Mastende/src/main/java/backend/database/Property.java
+++ Mastende/src/main/java/backend/database/Property.java
@@ -5,8 +5,9 @@ import java.sql.SQLException;
 public interface Property {
     //void numberofRooms(int room_numbers) throws SQLException;
     void addProperty_info(String property_name,int number_of_rooms, int rent, String address,String contact);
-    void roomStatus(String name, int room_no, String move_in_date, String employment_status, String cellphone,String pay_day,String room_price,String kin_name,String kin_number);
-
+    void roomStatus(String name, int room_no, String move_in_date, String employment_status,
+                    String cellphone,String pay_day,String room_price,int debt,
+                    String kin_name,String kin_number);
 
 }
 
