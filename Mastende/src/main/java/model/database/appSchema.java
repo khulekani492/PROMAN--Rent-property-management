c2d5d52230db3acd643830195305f85f5dab5949
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-09-27 23:38:24 +0200

    Main schemas for the entire application set, Users table,tenants , property and gross_income table

--- Mastende/src/main/java/model/database/appSchema.java
+++ Mastende/src/main/java/model/database/appSchema.java
@@ -1,10 +1,77 @@
 package model.database;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class appSchema extends connectionAcess {
     public appSchema(Connection connect) {
         super(connect);
     }
 
+    private void copySchema() throws SQLException {
+        try (Statement stmt = connection.createStatement()) {
+
+            stmt.execute("PRAGMA foreign_keys = ON;");
+
+            String Users = """
+                         CREATE TABLE IF NOT EXISTS Users (
+                                     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                     user_name TEXT NOT NULL,
+                                     user_email TEXT,
+                                     password TEXT NOT NULL,
+                                     propertyId INTEGER NULL,
+                                     FOREIGN KEY (propertyId) REFERENCES property(id)
+                                 );
+                    """;
+
+            String residenceTable = """
+                         CREATE TABLE IF NOT EXISTS property (
+                                     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                     property_name TEXT NOT NULL,
+                                     number_of_rooms INTEGER NOT NULL,
+                                     rent INTEGER NOT NULL,
+                                     address TEXT,
+                                     contact TEXT,
+                                     UserId INTEGER,
+                                     FOREIGN KEY (UserId) REFERENCES Users(id)
+                                     );
+                    """;
+
+
+            String tenantsTable = """
+                           CREATE TABLE IF NOT EXISTS tenants (
+                                       id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                       propertyId INTEGER NOT NULL,
+                                       name TEXT,
+                                       move_in DATE NOT NULL,
+                                       move_out DATE,
+                                       employment TEXT NOT NULL,
+                                       cell_number TEXT NOT NULL,
+                                       pay_day DATE NOT NULL,
+                                       room_number INTEGER NOT NULL,
+                                       Room_price INTEGER,
+                                       debt INTEGER,
+                                       kin_name Text,
+                                       kin_number Text,
+                                       FOREIGN KEY (propertyId) REFERENCES property(id)
+                                   );
+                    """;
+
+            String grossIncome = """
+                           CREATE TABLE IF NOT EXISTS gross_income (
+                                       id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                       propertyId INTEGER NOT NULL,
+                                       month_name TEXT,
+                                       Profit TEXT,
+                                       Loss TEXT,
+                                       FOREIGN KEY (propertyId) REFERENCES Users(id)
+                                   );
+                    """;
+            stmt.execute(Users);
+            stmt.execute(tenantsTable);
+            stmt.execute(residenceTable);
+            stmt.execute(grossIncome);
+        }
+    }
 }
