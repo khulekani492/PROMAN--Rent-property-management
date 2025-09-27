da1421a8e604182b593082bf21364c5b1ccf1050
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-09-27 16:52:30 +0200

    new DAO class landlord for the user information like email and password,property

---
+++ Mastende/src/main/java/model/database/landlord.java
@@ -0,0 +1,13 @@
+package model.database;
+
+public class landlord implements Property{
+    @Override
+    public void insert_information() {
+
+    }
+
+    @Override
+    public Integer UniqueID() {
+        return 0;
+    }
+}
