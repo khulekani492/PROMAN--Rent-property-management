316cb81b23a0ebf4e37fa821a91e310b1f2eb04e
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-09-27 17:12:51 +0200

    inheritance of the connectionAccess constructor

--- Mastende/src/main/java/model/database/landlord.java
+++ Mastende/src/main/java/model/database/landlord.java
@@ -1,6 +1,14 @@
 package model.database;
 
-public class landlord implements Property{
+import java.sql.Connection;
+
+public class landlord extends connectionAcess implements Property{
+
+
+    public landlord(Connection connect) {
+        super(connect);
+    }
+
     @Override
     public void insert_information() {
 
