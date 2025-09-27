0200702cd6d8a1f4850595fa2fc5374bb82fc941
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-09-27 22:55:19 +0200

    Set the first four preparedStatement set

--- Mastende/src/main/java/model/database/Tenant.java
+++ Mastende/src/main/java/model/database/Tenant.java
@@ -1,26 +1,28 @@
 package model.database;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
 public class Tenant extends  connectionAcess implements  Property{
+    private final int propertyId;
     private final String name;
     private final LocalDate moveInDate ;
     private final LocalDate moveOut;
     private final String employment;
     private final String cell_number;
     private final String pay_day;
-    private final  int room_number;
-    private final  int  room_price;
-    private final  int debt;
-    private final  String kin_name;
-    private final  String kin_number;
+    private final int room_number;
+    private final int  room_price;
+    private final int debt;
+    private final String kin_name;
+    private final String kin_number;
 
 
 
-    public Tenant(Connection connect, String name, LocalDate moveInDate,LocalDate moveOut,String employment,String cell_number,
+    public Tenant(Connection connect,int propertyId, String name, LocalDate moveInDate,LocalDate moveOut,String employment,String cell_number,
                   String pay_day,int room_number,int room_price,int debt,String kin_name,String kin_number) {
         super(connect);
         this.name = name;
@@ -34,6 +36,7 @@ public class Tenant extends  connectionAcess implements  Property{
         this.debt = debt;
         this.kin_name = kin_name;
         this.kin_number = kin_number;
+        this.propertyId =  propertyId;
     }
 
     @Override
@@ -42,9 +45,10 @@ public class Tenant extends  connectionAcess implements  Property{
                  INSERT INTO tenants (propertyId,name,move_in,move_out,employment,cell_number,pay_day,room_number,Room_price,debt,kin_name,kin_number)
                  """;
          try (PreparedStatement pstm = connection.prepareStatement(addtenantSql)){
-             pstm.setString(1,this.user_name);
-             pstm.setString(2,this.user_email);
-             pstm.setString(3,this.password);
+             pstm.setInt(1,this.propertyId);
+             pstm.setString(2,this.name);
+             pstm.setDate(3, Date.valueOf(this.moveInDate));
+             pstm.setDate(4, Date.valueOf(this.moveOut));
              pstm.executeUpdate();
 
          }catch (SQLException e) {
