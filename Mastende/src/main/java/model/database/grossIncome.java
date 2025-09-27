33d210c976296eae17aaa0d763e26969a537433e
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-09-27 23:50:51 +0200

    more logic thought listed on the application logic

--- Mastende/src/main/java/model/database/grossIncome.java
+++ Mastende/src/main/java/model/database/grossIncome.java
@@ -2,9 +2,9 @@ package model.database;
 
 import java.sql.Connection;
 
-//TODO logic for saving the money_made by the propertY in a particular month
-//TODO Involves View -- controller holding information aboout tenant payment set a trigger to be the first day of the month
-// Controller trigger-- send the update to the Model
+//TODO logic for saving the money_made by the property in a particular month
+//TODO Involves View -- controller holding information about tenant payment set a trigger to be the first day of the month
+// Controller trigger-- send the updates to the Model --View update the dashboard
 // --Model is this class grossincome stores the month that ended gross Income
 public class grossIncome extends  connectionAcess{
     public grossIncome(Connection connect) {
