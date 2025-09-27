82d47587aad24ec22549344e513cc0758d117aa9
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-09-27 20:04:08 +0200

    set residence constructor to take in tenants parameters

--- Mastende/src/main/java/model/database/residence.java
+++ Mastende/src/main/java/model/database/residence.java
@@ -1,19 +1,44 @@
 package model.database;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class residence extends connectionAcess implements  Property{
-    public residence(Connection connect) {
+    private String property_name;
+    private int number_of_rooms;
+    private int rent;
+    private String address;
+    private String contact;
+
+    public residence(Connection connect, String property_name, int number_of_rooms,int rent,String address, String contact) {
         super(connect);
+        this.property_name = property_name;
+        this.number_of_rooms = number_of_rooms;
+        this.rent = rent;
+        this.address = address;
+        this.contact = contact;
+
     }
 
     @Override
     public void insert_information() {
+        String propertySQL = """
+            INSERT INTO property (property_name, number_of_rooms, rent, address, contact,UserId)
+            VALUES (?, ?, ?, ?, ?)
+          """;
+        try (PreparedStatement pstmt = connector.prepareStatement(propertySQL)) {
+            pstmt.setString(1, property_name);
+            pstmt.setInt(2, number_of_rooms);
+            pstmt.setInt(3, rent);
+            pstmt.setString(4, address);
+            pstmt.setString(5, contact);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
-
-
-
 
     @Override
     public Integer UniqueID(String name) {
