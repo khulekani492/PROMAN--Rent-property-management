2ce4ba9bd6409bcaa6bff9a0d55a96eb3c060436
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-09-27 17:09:01 +0200

    abtract class for the datasource

---
+++ Mastende/src/main/java/model/database/connectionAcess.java
@@ -0,0 +1,11 @@
+package model.database;
+
+import java.sql.Connection;
+
+public abstract class connectionAcess {
+    protected Connection connection;
+
+    public connectionAcess(Connection connect){
+        this.connection = connect;
+    }
+}
