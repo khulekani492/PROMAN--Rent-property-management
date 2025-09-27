23c2dff6d2e7a76a61d9e7807dd1b66d0279ae45
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-09-27 23:32:23 +0200

    appschema foundation of the application Data, the tables and and their respective schema

---
+++ Mastende/src/main/java/model/database/appSchema.java
@@ -0,0 +1,10 @@
+package model.database;
+
+import java.sql.Connection;
+
+public class appSchema extends connectionAcess {
+    public appSchema(Connection connect) {
+        super(connect);
+    }
+
+}
