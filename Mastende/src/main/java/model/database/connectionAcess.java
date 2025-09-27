dd53dad818a153c883d2d938902de0b8b6773121
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-09-28 01:37:23 +0200

    parent class has a default database url all subclasses will inherit

--- Mastende/src/main/java/model/database/connectionAcess.java
+++ Mastende/src/main/java/model/database/connectionAcess.java
@@ -1,12 +1,14 @@
 package model.database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public abstract class connectionAcess {
-
+    protected static final String DB_URL = "jdbc:sqlite:properties.db";
     protected Connection connection;
 
-    public connectionAcess(Connection connect){
-        this.connection = connect;
+    public connectionAcess() throws SQLException {
+        this.connection = DriverManager.getConnection(DB_URL);
     }
 }
