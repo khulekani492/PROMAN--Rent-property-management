19ac46166b23461e785a587e0dd5dc7e33bd02af
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-10-05 01:19:38 +0200

    updates on the residence inputs due to the new column

--- Mastende/src/main/java/model/main.java
+++ Mastende/src/main/java/model/main.java
@@ -12,11 +12,6 @@ public class main {
 
     public static void main(String[] args) throws SQLException {
         appSchema checkschema = new appSchema();
-        landlord mastede = new landlord("Khulekani","khulekaniszondo6@gmail.com","AlphaOmega");
-        mastede.insert_information();
-        int asibone = mastede.UniqueID();
-        residence mastede_property = new residence("Alexandra",20,1000,"Njabulani","0822690384",asibone);
-        mastede_property.insert_information();
 
     }
 }
