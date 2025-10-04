dabf2d2d25de26715eb65ab43dc205e5253b5477
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-10-05 01:54:53 +0200

    new dummy db connection

--- Mastende/src/main/java/model/database/connectionAcess.java
+++ Mastende/src/main/java/model/database/connectionAcess.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public abstract class connectionAcess {
-    protected static final String DB_URL = "jdbc:sqlite:propertYTEST.db";
+    protected static final String DB_URL = "jdbc:sqlite:dummytwo.db";
     protected Connection connection;
 
     public connectionAcess() throws SQLException {
