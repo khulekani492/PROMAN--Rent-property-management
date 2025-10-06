eddc9dd593c0233493383a8cd05bab6d55fc6751
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-10-06 12:54:26 +0200

    new class for keeping track of room number chosen or specefied to have a tenant

--- Mastende/src/main/java/API/state.java
+++ Mastende/src/main/java/API/state.java
@@ -1,4 +1,18 @@
 package API;
 
 public class state {
+    private Integer current_track;
+
+    public  state(Integer current_track){
+        this.current_track = current_track;
+    }
+
+    public Integer getCurrent_track(){
+        return  this.current_track;
+    }
+
+    public void setCurrent_track(Integer current_track){
+        this.current_track =  current_track;
+
+    }
 }
