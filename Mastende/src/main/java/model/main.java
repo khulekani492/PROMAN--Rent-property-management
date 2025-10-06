d433852aaa94b5797cfa0fd8cf658906afaa2da5
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-10-06 22:53:36 +0200

    dummy insertion to test connection

--- Mastende/src/main/java/model/main.java
+++ Mastende/src/main/java/model/main.java
@@ -1,17 +1,19 @@
 package model;
 
-import model.database.Tenant;
-import model.database.appSchema;
-import model.database.landlord;
-import model.database.residence;
+import model.database.*;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class main {
 
-    public static void main(String[] args) throws SQLException {
-        appSchema checkschema = new appSchema();
-
-    }
-}
+    public static void main(String[] args) {
+        try {
+            landlord news = new landlord("khulekani","khulekaniszondo@gmail.com","that96");
+            news.insert_information();
+            System.out.println("Connected successfully!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }}
