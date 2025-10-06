630db7f121a28d0c642594eb2cee7282881876d3
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-10-07 01:28:21 +0200

    data type for pay_day updated to Date type

--- Mastende/src/main/java/model/database/roomHist.java
+++ Mastende/src/main/java/model/database/roomHist.java
@@ -1,5 +1,6 @@
 package model.database;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -8,12 +9,12 @@ public class roomHist extends connectionAcess implements Property  {
     private final Integer roomNo;
     private final String  tenantname;
     private final String contact;
-    private final  String move_in;
-    private final  String move_out;
+    private final Date move_in;
+    private final  Date move_out;
 
 
 
-    public roomHist(Integer propertyId, Integer room, String tenantname, String contact, String moveIn, String moveOut) throws SQLException {
+    public roomHist(Integer propertyId, Integer room, String tenantname, String contact, Date moveIn, Date moveOut) throws SQLException {
         super();
         this.propertyId = propertyId;
         this.roomNo = room;
@@ -26,14 +27,14 @@ public class roomHist extends connectionAcess implements Property  {
     @Override
     public void insert_information() {
         String propertySQL = """
-    INSERT INTO per_room (propertyId, tenant_name, move_in, cellphone_number, room_number)
+    INSERT INTO room_history (propertyId, tenant_name, move_in, cellphone_number, room_number)
     VALUES (?, ?, ?, ?, ?)
 """;
 
         try (PreparedStatement pstmt = connection.prepareStatement(propertySQL)) {
             pstmt.setInt(1, this.propertyId);
             pstmt.setString(2, this.tenantname);
-            pstmt.setString(3,this.move_in);
+            pstmt.setDate(3,this.move_in);
             pstmt.setString(4,this.contact);
             pstmt.setInt(5, this.roomNo);
             pstmt.executeUpdate();
