fb7c0f08ba3e6148d2d968cb3436335f0385365b
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-10-08 20:33:06 +0200

    tests getters for the constructor

--- Mastende/src/test/java/model/database/LandlordTest.java
+++ Mastende/src/test/java/model/database/LandlordTest.java
@@ -13,9 +13,9 @@ class LandlordTest {
     Landlord newLandlord = new Landlord("mfokaMkhize","mkhize@2456@gmail.com","TightSecurity");
 
 
-    assertEquals("mfokaMkhize", landlord.getUser_name());
-    assertEquals("mkhize@2456@gmail.com", landlord.getUser_email());
-    assertEquals("TightSecurity", landlord.getPassword());
+    assertEquals("mfokaMkhize", newLandlord.getUser_name());
+    assertEquals("mkhize@2456@gmail.com", newLandlord.getUser_email());
+    assertEquals("TightSecurity", newLandlord.getPassword());
 }
 
 }
