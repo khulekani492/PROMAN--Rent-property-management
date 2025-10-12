57da38aff19e189e90123b4c6423d3f04d8120c9
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-10-13 00:18:59 +0200

    commit

--- Mastende/src/main/java/model/main.java
+++ Mastende/src/main/java/model/main.java
@@ -10,15 +10,11 @@ import java.sql.SQLException;
 public class main {
 
     public static void main(String[] args) {
-        try {
-            residence newoens = new  residence("Sunset Villas",20,4,5000,"lase","03283833",1);
-            newoens.insert_information();
-            Tenant newss = new Tenant(1,  "KILR",  Date.valueOf("2025-10-06"),   "yes",  "0823343233",  Date.valueOf("2020-10-06"), 5, 4500,  "d","09883888394"
-            );
-            newss.insert_information();
-            System.out.println("Tenant inserted successfully!");
+        //            residence newoens = new  residence("Sunset Villas",20,4,5000,"lase","03283833",1);
+//            newoens.insert_information();
+//            Tenant newss = new Tenant(1,  "KILR",  Date.valueOf("2025-10-06"),   "yes",  "0823343233",  Date.valueOf("2020-10-06"), 5, 4500,  "d","09883888394"
+//            );
+//            newss.insert_information();
+        System.out.println("Tenant inserted successfully!");
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }}
