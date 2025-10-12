b0423a1a64148424f587a26d9dd66cb4b8773b60
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-10-13 00:18:22 +0200

    testing github action detects failure

--- Mastende/src/test/java/model/database/generalTest.java
+++ Mastende/src/test/java/model/database/generalTest.java
@@ -179,7 +179,7 @@ public class generalTest {
         assertEquals(price, property.getDebt(), "Debt should match the inserted value");
 
         Integer moneyOwed = property.queryDebt();
-        assertNotNull(moneyOwed, "Queried debt should not be null");
+        assertNull(moneyOwed, "Queried debt should not be null");
         assertEquals(property.getDebt(), moneyOwed, "Queried debt should match the inserted value");
 
 
