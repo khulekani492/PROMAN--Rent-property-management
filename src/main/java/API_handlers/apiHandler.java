40f8c55acd0bad4191778237e817bcd997e65bd0
khzondojhb024 <khzondojhb024@student.wethinkcode.co.za>
2025-10-15 16:09:11 +0200

    updates

--- src/main/java/API_handlers/apiHandler.java
+++ src/main/java/API_handlers/apiHandler.java
@@ -2,7 +2,7 @@ package API_handlers;
 
 import API.state;
 import io.javalin.http.Handler;
-import model.database.Tenant;
+import model.database.tenant;
 import model.database.counter;
 import model.database.general;
 import model.database.residence;
@@ -26,12 +26,12 @@ public class apiHandler {
                 String email = ctx.formParam("user_email");
                 String password = ctx.formParam("password");
                 String user_type = ctx.formParam("user_type");
-                Integer numberofUnits = Integer.valueOf(ctx.formParam("n_units"));
+
                 // hash password
                 String hashedPassword = hashPassword(password);
 
                 // insert user into database
-                general new_user = new general(user_name, contact, email, hashedPassword,user_type,property_address,numberofUnits);
+                general new_user = new general(user_name, contact, email, hashedPassword,user_type,property_address);
                 new_user.insert_information();
 
                 // get new user ID
@@ -63,22 +63,15 @@ public class apiHandler {
            try{
             Integer propertyUnit = Integer.valueOf(ctx.formParam("property_name"));
             ctx.sessionAttribute("propertyname",propertyUnit);
-           // Integer numberOfRooms =  Integer.parseInt(Objects.requireNonNull(ctx.formParam("number_of_rooms")));
             Integer rent =  Integer.parseInt(Objects.requireNonNull(ctx.formParam("rent")));
-
-           /// Integer OccupiedRooms =  Integer.parseInt(Objects.requireNonNull(ctx.formParam("roomsOccupied")));
-            //ctx.sessionAttribute("roomsOccupied",OccupiedRooms);
-
             String occupation = ctx.formParam("occupation");
             //String contact = ctx.formParam("contact");
-            //Get the new user unique_Id by accessing the key-value setAttributes() session set in /user_sign_up Url_end point
             Integer property_owner = ctx.sessionAttribute("user_ID");
 
             //Insert into property table property_information
             residence property_information = new residence(propertyUnit,rent,occupation);
-            property_information.setlandlord(ctx.sessionAttribute("user_ID"));
+            property_information.setlandlord(property_owner);
             property_information.insert_information();
-
             //entity relationship with the Users table
             Integer owner_property = property_information.UniqueID();
             System.out.println("owner_property");
@@ -156,15 +149,9 @@ public class apiHandler {
                 String kin_number = ctx.formParam("kin_number");
 
                 //additional information about the tenant --> tenants_information table
-                Tenant additional_information = new Tenant(moveIn,employment_status,kin_name,kin_number);
+                tenant additional_information = new tenant(moveIn,employment_status,kin_name,kin_number);
                 additional_information.setTenantId(tenantUniqueID);
                 additional_information.insert_information();
-
-
-
-               // Tenant tenant = new Tenant(moveIn,cell_number,payday,employment_status,kin_name,kin_number);
-                //tenant.insert_information();
-
                 /**
                  * If the counter equals the number of rooms, render the user_profile HTML.
                  * Otherwise, keep rendering the tenant_form.
