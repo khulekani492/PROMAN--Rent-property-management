abbf3465e77f6558e68f820e3ad7979899e3bb66
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-10-30 09:55:16 +0200

    main class to run to start up the server

--- src/main/java/org/sonarEffect/Main.java
+++ src/main/java/org/sonarEffect/Main.java
@@ -2,11 +2,8 @@ package org.sonarEffect;
 import java.sql.SQLException;
 
 import static API.umuziAPI.startServer;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    static void main(String[] args) throws SQLException {
         startServer(7070);
     }
 }
