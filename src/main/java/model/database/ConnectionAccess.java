51af98b05b4457113e684e8a863c5199d6062c6d
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-01 10:29:58 +0200

    new abstract class that get the singleton connection and share it across all it subclass

---
+++ src/main/java/model/database/ConnectionAccess.java
@@ -0,0 +1,12 @@
+package model.database;
+
+import java.sql.Connection;
+
+public abstract class ConnectionAccess {
+    protected Connection connection;
+
+    public ConnectionAccess  () {
+        this.connection = Database_access.getInstance().getConnection();
+    }
+
+}
