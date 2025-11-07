82a473edae3c15215b9b43b78fc4d23d57f43cf9
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-07 08:59:49 +0200

    bug Fixed --> address succefully added to the table

--- src/main/java/org/sonarEffect/Main.java
+++ src/main/java/org/sonarEffect/Main.java
@@ -1,7 +1,7 @@
 package org.sonarEffect;
 import java.sql.SQLException;
-
 import static API.umuziAPI.startServer;
+
 public class Main {
     public static void main(String[] args) throws SQLException {
         startServer(7070);
