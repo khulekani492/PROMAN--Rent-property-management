f490c2355a9b3e3eca3cae765880222abb566c75
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-08 09:32:58 +0200

    factory class

--- src/main/java/Invalidhandler/UpdateUser.java
+++ src/main/java/Invalidhandler/UpdateUser.java
@@ -1,4 +1,12 @@
 package Invalidhandler;
 
-public class UpdateUser {
+ public abstract class UpdateUser {
+
+    public abstract ErrorHandler error_handler();
+
+    public void updateUser(String message){
+        ErrorHandler update_user = error_handler();
+        update_user.error_message(message);
+    }
+
 }
