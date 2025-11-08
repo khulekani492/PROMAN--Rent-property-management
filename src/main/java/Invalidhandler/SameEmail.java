81c328bcedeb16e3a7d1bd8d50b100f2569cecd0
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-08 09:31:15 +0200

    Implementing the Errorhandler interface for same email exceptions

--- src/main/java/Invalidhandler/SameEmail.java
+++ src/main/java/Invalidhandler/SameEmail.java
@@ -1,4 +1,36 @@
 package Invalidhandler;
 
-public class SameEmail {
+import io.javalin.http.Handler;
+
+import java.util.HashMap;
+
+public class SameEmail implements ErrorHandler{
+
+
+
+    public Handler error_message3() {
+        return ctx -> {
+            String  update_user = ctx.sessionAttribute("error");
+            //ErrorHandler  updateUser = new SameUnit;
+            //updateUser.update(Update)
+            String property_name = ctx.sessionAttribute("property_name");
+            Integer property_unit = ctx.sessionAttribute("property_unit");
+            String property_address = ctx.sessionAttribute("property address");
+            String property_occupation = ctx.sessionAttribute("occupation");
+
+            HashMap<String,String> model = new HashMap<>();
+            model.put("error",update_user);
+            model.put("property_name",property_name);
+            model.put("property_unit", String.valueOf(property_unit));
+            model.put("property_address",property_address);
+            model.put("property_occupation",property_occupation);
+            ctx.render("templates/property.html",model);
+            ctx.redirect("/error/{message}");
+        };
+    }
+
+    @Override
+    public void  error_message(String message) {
+        System.out.println(message +  " Log in or create another account " );
+    }
 }
