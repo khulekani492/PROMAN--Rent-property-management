a98987ba733b6da5e2a8684ebe51fd523fbf4fcb
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-08 09:32:19 +0200

    factory class

--- src/main/java/Invalidhandler/SameEmialCreator.java
+++ src/main/java/Invalidhandler/SameEmialCreator.java
@@ -1,4 +1,8 @@
 package Invalidhandler;
 
-public class SameEmialCreator {
+public class SameEmialCreator extends UpdateUser{
+    @Override
+    public ErrorHandler error_handler() {
+        return new SameEmail();
+    }
 }
