8a2f08e322e3ccee9f0f746363dbb917a70108d8
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-10 15:43:48 +0200

    Bug Fixed ---> rent_payment "Wrong conversion of data type "

--- src/main/java/model/database/ConnectionAccess.java
+++ src/main/java/model/database/ConnectionAccess.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 
 public abstract class ConnectionAccess {
     protected Connection connection;
-
     public ConnectionAccess  () {
         this.connection = Database_access.getInstance().getConnection();
     }
