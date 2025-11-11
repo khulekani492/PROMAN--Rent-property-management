cf7c90da02603db76d304fb066340ad9e568de4a
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-11 20:56:21 +0200

    returns a list of the units of the properties BUG fixed replace a hard predetermined for loop with looping through the list to get the actual values of the key

--- src/main/java/model/database/CRUD/Getunits.java
+++ src/main/java/model/database/CRUD/Getunits.java
@@ -9,21 +9,15 @@ public class Getunits {
         Propertyinfo property_list = new Propertyinfo();
 
         HashMap<Integer,ArrayList<String>> get_Unit_related = property_list.property_tenants(property_name);
-        System.out.println(get_Unit_related + " Amen");
         HashMap<Integer,ArrayList<String>> get_tenants = new HashMap<>();
-        ArrayList<String> property_per_unit ;
-        for (int i =1 ; i <= get_Unit_related.size() ; i++){
-            try {
-                 property_per_unit = get_Unit_related.get(i);
-            } catch (NullPointerException e) {
-                System.out.println("My message " + e.getMessage());
-                throw new RuntimeException(e);
-            }
+        ArrayList<String> property_per_unit  = new ArrayList<>();
+
+        for (Integer n : get_Unit_related.keySet()){
+             property_per_unit = get_Unit_related.get(n);
 
 
             Integer AccessLast = null;
             //Skip unit with no tenants == null on index 3
-            System.out.println(property_per_unit);
             try {
                 AccessLast = Integer.valueOf(property_per_unit.get(3));
             } catch (NullPointerException e){
@@ -43,14 +37,9 @@ public class Getunits {
                 }
 
             }
-
             //adding the rent
             property_per_unit.add(rent_day_tenant);
-
             get_tenants.put(first_one,property_per_unit);
-
-
-
         }
         return get_tenants;
     }
