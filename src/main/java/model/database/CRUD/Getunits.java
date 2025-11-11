c699bc5dd1f421ae8afdb9514db3a1e1d7b1cdd7
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-11 17:47:45 +0200

    Error handling for null values when looking for units with tenants

--- src/main/java/model/database/CRUD/Getunits.java
+++ src/main/java/model/database/CRUD/Getunits.java
@@ -1,5 +1,6 @@
 package model.database.CRUD;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -8,14 +9,25 @@ public class Getunits {
         Propertyinfo property_list = new Propertyinfo();
 
         HashMap<Integer,ArrayList<String>> get_Unit_related = property_list.property_tenants(property_name);
+        System.out.println(get_Unit_related + " Amen");
         HashMap<Integer,ArrayList<String>> get_tenants = new HashMap<>();
+        ArrayList<String> property_per_unit ;
         for (int i =1 ; i <= get_Unit_related.size() ; i++){
-            ArrayList<String> property_per_unit = get_Unit_related.get(i);
+            try {
+                 property_per_unit = get_Unit_related.get(i);
+            } catch (NullPointerException e) {
+                System.out.println("My message " + e.getMessage());
+                throw new RuntimeException(e);
+            }
+
 
             Integer AccessLast = null;
             //Skip unit with no tenants == null on index 3
+            System.out.println(property_per_unit);
             try {
                 AccessLast = Integer.valueOf(property_per_unit.get(3));
+            } catch (NullPointerException e){
+                continue;
             } catch (NumberFormatException e){
                 continue;
             }
@@ -24,7 +36,8 @@ public class Getunits {
             String rent_day_tenant = null;
             try {
                 rent_day_tenant = property_list.rent_payment(AccessLast);
-            } catch (Exception e) {
+            } catch (RuntimeException e) {
+                System.out.println("Erorr Message " + e.getMessage());
                 if("skip".equals(e.getMessage())){
                     continue;
                 }
