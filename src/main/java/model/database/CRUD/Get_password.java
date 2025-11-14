9d585f89f3d37c5887dc02d6fe52dc03c4ee6376
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-14 15:34:00 +0200

    Class that query password for user_email

---
+++ src/main/java/model/database/CRUD/Get_password.java
@@ -0,0 +1,51 @@
+package model.database.CRUD;
+
+import model.database.ConnectionAccess;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Get_password extends ConnectionAccess {
+    private  String user_email;
+
+
+    public Get_password(String user_email){
+        this.user_email = user_email;
+
+
+    }
+
+    public String getUser_email(){
+        return this.user_email;
+    }
+
+    public String compare() {
+        String fetch = "SELECT password FROM general_users WHERE email = ?";
+
+        try {
+            PreparedStatement pstm = this.connection.prepareStatement(fetch);
+            pstm.setString(1, this.user_email);
+
+            ResultSet result = pstm.executeQuery();
+
+            if (result.next()) {
+                String pass = result.getString("password");
+                return pass;
+            } else {
+                    return  "NO USER FOUND";
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static void main(String[] args){
+        Get_password raw = new Get_password("khulekaniszondo6@gmail.com");
+
+        System.out.println(raw.compare());
+        System.out.println(raw.getUser_email());
+    }
+
+}
