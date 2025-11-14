7a92ebd558571146d2a2128d5e1812c69b2b1532
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-14 17:18:46 +0200

    Login page

--- src/main/java/API_handlers/Validate_login.java
+++ src/main/java/API_handlers/Validate_login.java
@@ -1,6 +1,7 @@
 package API_handlers;
 
 import io.javalin.http.Handler;
+import model.database.CRUD.Get_password;
 
 import static API.SecurityUtil.checkPassword;
 import static API.SecurityUtil.hashPassword;
@@ -11,12 +12,17 @@ public class Validate_login {
         return ctx -> {
             String user_email = ctx.formParam("email");
             String password = ctx.formParam("password");
-
-            //
-            String hashedPassword = "";
-
-            boolean isMatch = checkPassword(password, hashedPassword);
-            System.out.println("Password matches: " + isMatch);
+            System.out.println(user_email);
+            Get_password hashed = new Get_password(user_email);
+            System.out.println(hashed);
+            System.out.println("b" + hashed.compare());
+            boolean isMatch = checkPassword(password, hashed.compare() );
+            if (isMatch){
+                ctx.sessionAttribute("email",user_email);
+                ctx.redirect("dashboard");
+            } else {
+                ctx.result("wrong password");
+            }
 
         };
 
