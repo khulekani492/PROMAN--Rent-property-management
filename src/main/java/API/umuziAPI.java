35e627ba10c264a57271630a92a9b808432058f2
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-14 11:55:37 +0200

    includes two HashMaps

--- src/main/java/API/umuziAPI.java
+++ src/main/java/API/umuziAPI.java
@@ -78,21 +78,37 @@ public class umuziAPI {
             System.out.println("property_landlord " +authenticate.landlordId(property_email) );
             HashMap<Integer, ArrayList<String>> fetch_all = property_units.getOccupiedUnits(property_name,authenticate.landlordId(property_email));
             System.out.println("RESULTS : " + fetch_all);
+
             if(fetch_all.size() == 0){
                 HashMap<String,String> model = new HashMap<>();
                 String property = context.sessionAttribute("property_name");
-                model.put("no_units","No units added for " + property );
+                model.put("no_units","No units added for " + property);
                 context.render("templates/dashboard.html",model);
             }else {
+                Map<String, Object> data = new HashMap<>();
+
+
                 HashMap<String,HashMap<Integer,ArrayList<String>>> model = new HashMap<>();
+                ;
+                Map<String, Object> model1 = new HashMap<>();
+
                 System.out.println(fetch_all + "Occupied units");
                 model.put("units",fetch_all);
                 System.out.println(model);
                 System.out.println(fetch_all);
+                //using landlord_unique_id to fetch all of their properties ,it accessed with the user_email
+                String email = context.sessionAttribute("email");
+                propertyNames default_properties = new propertyNames();
+                landlord user_id = new landlord();
+                Set<String> landlord_properties = default_properties.fetchAllproperty(user_id.landlordId(email));
 
-                context.render("templates/dashboard.html",model);
+                model1.put("names",landlord_properties);
+                data.putAll(model);
+                data.putAll(model1);
+                System.out.println("woah");
+                System.out.println(data);
+                context.render("templates/dashboard.html",data);
             }
-
         });
 
         app.get("/property_information", new PropertyUnits().property_related_information());
