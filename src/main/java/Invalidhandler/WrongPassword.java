9a85f67373a6fd7254ccf118ed4a346bd263ef9b
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-15 11:38:41 +0200

    factory method for handling wrong password

--- src/main/java/Invalidhandler/WrongPassword.java
+++ src/main/java/Invalidhandler/WrongPassword.java
@@ -1,4 +1,19 @@
 package Invalidhandler;
 
-public class WrongPassword {
+import io.javalin.http.Handler;
+
+import java.util.HashMap;
+
+public class WrongPassword implements  ErrorHandler {
+
+    @Override
+    public Handler error_message() {
+        return ctx -> {
+            String attempted_email = ctx.sessionAttribute("login_email");
+            HashMap<String,String> model = new HashMap<>();
+            model.put("wrong", " password Incorrect"   );
+            model.put("login_email", attempted_email );
+            ctx.render("templates/login.html",model);
+        };
+    }
 }
