363485b59f16caceb724a1eafef1cfee0d52383b
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-15 10:55:10 +0200

    factory method handles what will be displayed to the user

--- src/main/java/Invalidhandler/NoEmail.java
+++ src/main/java/Invalidhandler/NoEmail.java
@@ -1,4 +1,19 @@
 package Invalidhandler;
 
-public class NoEmail {
+import io.javalin.http.Handler;
+
+import java.util.HashMap;
+
+public class NoEmail implements ErrorHandler {
+    @Override
+    public Handler error_message() {
+        return ctx -> {
+            String attempted_email = ctx.sessionAttribute("login_email");
+            System.out.println(attempted_email);
+            HashMap<String,String> model = new HashMap<>();
+            model.put("error", attempted_email + " EMAIL NOT FOUND"   );
+            model.put("Create","Sign up");
+            ctx.render("templates/login.html",model);
+        };
+    }
 }
