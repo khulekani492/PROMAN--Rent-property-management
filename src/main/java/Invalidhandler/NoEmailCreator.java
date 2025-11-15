cc0eaf199ff4c0acff8854c84b2bd317662d4368
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-15 10:55:55 +0200

    factory method handles what will be displayed to the user

--- src/main/java/Invalidhandler/NoEmailCreator.java
+++ src/main/java/Invalidhandler/NoEmailCreator.java
@@ -1,4 +1,10 @@
 package Invalidhandler;
 
-public class NoEmailCreator {
+import io.javalin.http.Handler;
+
+public class NoEmailCreator extends UpdateUser {
+    @Override
+    public ErrorHandler error_handler() {
+        return new NoEmail();
+    }
 }
