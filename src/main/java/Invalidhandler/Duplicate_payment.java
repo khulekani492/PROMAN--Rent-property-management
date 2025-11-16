3d2612e86d7d7ef69b945358e699922232c633a1
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-16 21:12:27 +0200

    Error handler for Duplicate_payment

---
+++ src/main/java/Invalidhandler/Duplicate_payment.java
@@ -0,0 +1,21 @@
+package Invalidhandler;
+
+import io.javalin.http.Handler;
+
+import java.util.HashMap;
+
+public class Duplicate_payment implements ErrorHandler {
+
+    @Override
+    public Handler error_message() {
+        return ctx ->{
+            String before_state = ctx.sessionAttribute("dashBoard_current_property");
+            System.out.println("Before state " + before_state);
+            String before_state_email = ctx.sessionAttribute("dashBoard_current_email");
+            System.out.println("Before state email " + before_state_email);
+            ctx.redirect("/unit_already_paid/" + before_state+"/"+before_state_email);
+
+
+        } ;
+    }
+}
