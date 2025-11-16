d6641826733c6fabb9732e33d1306b770c4550b5
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-16 15:22:15 +0200

    Model update Reminder class gets landlords list with tenants who are due to send out emails for them to set a status on the dashboard

---
+++ src/main/java/model/database/CRUD/Reminder.java
@@ -0,0 +1,39 @@
+package model.database.CRUD;
+
+import model.database.ConnectionAccess;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Reminder extends ConnectionAccess {
+
+    public Set<Integer> fetchAll(){
+        Set<Integer> landlords = new HashSet<>();
+     String sql = """
+             SELECT landlord_user_id FROM properties INNER JOIN tenants_information ON\s
+             properties.tenant_user_id = tenants_information.tenant_user_id WHERE tenants_information.status=false
+            \s""";
+
+     try(Statement pstm = connection.createStatement()){
+         ResultSet result = pstm.executeQuery(sql);
+         while (result.next()){
+              landlords.add( result.getInt("landlord_user_id")  ) ;
+
+         }
+
+     } catch (SQLException e) {
+         throw new RuntimeException(e);
+     }
+     return landlords ;
+
+    }
+public static void main(String[] args){
+        Reminder omastede = new Reminder();
+    System.out.println(omastede.fetchAll());
+}
+
+}
