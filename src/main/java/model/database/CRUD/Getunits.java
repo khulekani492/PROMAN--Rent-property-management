883c9050a34ba8343a1160cb31430db5e6311f65
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-16 14:03:35 +0200

    Model update rent_payment() method refactored to Finance()

--- src/main/java/model/database/CRUD/Getunits.java
+++ src/main/java/model/database/CRUD/Getunits.java
@@ -1,6 +1,5 @@
 package model.database.CRUD;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -11,7 +10,7 @@ public class Getunits {
         HashMap<Integer,ArrayList<String>> get_Unit_related = property_list.property_tenants(property_name,landlordId);
         HashMap<Integer,ArrayList<String>> get_tenants = new HashMap<>();
         ArrayList<String> property_per_unit  = null;
-
+        System.out.println(get_Unit_related);
         for (Integer n : get_Unit_related.keySet()){
              property_per_unit = get_Unit_related.get(n);
 
@@ -25,27 +24,27 @@ public class Getunits {
             } catch (NumberFormatException e){
                 continue;
             }
-            // Accessing the unit number which is Identical to the map key
+            // Accessing the unit number which is Identical to the map key[unit] first index,add rent to the last value
             Integer first_one = Integer.valueOf(property_per_unit.get(0));
-            String rent_day_tenant = null;
+            ArrayList<String> rent_day_tenant = new ArrayList<>();
+
             try {
-                rent_day_tenant = property_list.rent_payment(AccessLast);
+                rent_day_tenant = property_list.Finances(AccessLast);
             } catch (RuntimeException e) {
-                System.out.println("Erorr Message " + e.getMessage());
                 if("skip".equals(e.getMessage())){
                     continue;
                 }
 
             }
             //adding the rent
-            property_per_unit.add(rent_day_tenant);
+            property_per_unit.addAll(rent_day_tenant);
             get_tenants.put(first_one,property_per_unit);
         }
         return get_tenants;
     }
 static void main(){
         Getunits per_property = new Getunits();
-    System.out.println(per_property.getOccupiedUnits("Thornville_rooms",817)    ); ;
+    System.out.println(per_property.getOccupiedUnits("Thornville_rooms",771)    ); ;
 }
 
 }
