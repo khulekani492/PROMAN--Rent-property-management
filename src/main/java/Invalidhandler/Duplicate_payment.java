e58ea31c9756d8524dbdca5405423a55fd7d4e54
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-16 22:13:08 +0200

    added unit number in the url

--- src/main/java/Invalidhandler/Duplicate_payment.java
+++ src/main/java/Invalidhandler/Duplicate_payment.java
@@ -12,8 +12,9 @@ public class Duplicate_payment implements ErrorHandler {
             String before_state = ctx.sessionAttribute("dashBoard_current_property");
             System.out.println("Before state " + before_state);
             String before_state_email = ctx.sessionAttribute("dashBoard_current_email");
+            String unit = ctx.sessionAttribute("unit");
             System.out.println("Before state email " + before_state_email);
-            ctx.redirect("/unit_already_paid/" + before_state+"/"+before_state_email);
+            ctx.redirect("/unit_already_paid/" + before_state+"/"+before_state_email +"/" +unit);
 
 
         } ;
