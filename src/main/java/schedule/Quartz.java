b1e5b2163378bf58d60d4e3014bed8a412c87cae
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-17 17:26:23 +0200

    quartz

---
+++ src/main/java/schedule/Quartz.java
@@ -0,0 +1,36 @@
+package schedule;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.impl.StdScheduler;
+import org.quartz.impl.StdSchedulerFactory;
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class Quartz {
+      static void main() throws SchedulerException {
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+
+
+        JobDetail test_job = newJob(Reminder.class)
+                .withIdentity("emails","group1")
+                .build();
+
+        Trigger trigger = newTrigger().
+                withIdentity("my_trigger","group1")
+                .startNow()
+                .withSchedule(simpleSchedule()
+                        .withIntervalInSeconds(40)
+                        .repeatForever())
+                .build();
+
+        scheduler.scheduleJob(test_job,trigger);
+
+
+    }
+
+
+    }
