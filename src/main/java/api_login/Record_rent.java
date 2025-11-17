c89ed2865c3ca5408cd73c72e6d890797bdc9d0f
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-17 12:25:32 +0200

    Updated API includes url for successful payment_status -->Unit number|dashboard regenerates the html of the first state of the user session dashboard

--- src/main/java/api_login/Record_rent.java
+++ src/main/java/api_login/Record_rent.java
@@ -1,10 +1,16 @@
 package api_login;
 
 import io.javalin.http.Handler;
+import model.database.CRUD.Getunits;
+import model.database.CRUD.landlord;
+import model.database.CRUD.propertyNames;
 import model.database.Transaction;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Record_rent {
     public Handler payment(){
@@ -32,12 +38,62 @@ public class Record_rent {
                 throw new RuntimeException(e);
             }
 
-            HashMap<String,String> model = new HashMap<>();
+            HashMap<String,String> MODEL = new HashMap<>();
+
             try{
                 _payment.record_payment();
                 String unit_paid = ctx.sessionAttribute("unit");
-                model.put("success",unit_paid);
-                ctx.render("templates/dashboard.html",model);
+                MODEL.put("success",unit_paid);
+                MODEL.put("","");
+                //class clone jutsu DashBoard
+                String clone_property = ctx.pathParam("property_name");
+                String clone_email = ctx.pathParam("user_email");
+                String clone_unit = ctx.pathParam("id");
+                System.out.println(clone_unit + "Tenant ID");
+                Getunits property_units = new Getunits();
+                landlord authenticate = new landlord();
+
+                try{
+                    authenticate.landlordId(clone_email);
+                } catch (Exception e) {
+                    System.out.println("something wrong");
+                    throw new RuntimeException(e);
+                }
+                HashMap<Integer, ArrayList<String>> fetch_all = property_units.getOccupiedUnits(clone_property,authenticate.landlordId(clone_email));
+
+                if(fetch_all.size() == 0){
+
+                    String property = ctx.sessionAttribute("property_name");
+                    MODEL.put("no_units","No units added for " + property);
+                    ctx.render("templates/dashboard.html",MODEL);
+                }else {
+                    Map<String, Object> data = new HashMap<>();
+                    HashMap<String,HashMap<Integer,ArrayList<String>>> model = new HashMap<>();
+                    HashMap<String,String> model2 = new HashMap<>();
+                    Map<String, Object> model1 = new HashMap<>();
+                    System.out.println(fetch_all + "Occupied units");
+                    model.put("units",fetch_all);
+                    //using landlord_unique_id to fetch all of their properties ,it accessed with the user_email
+                    String email = ctx.sessionAttribute("email");
+                    propertyNames default_properties = new propertyNames();
+
+                    landlord user_id = new landlord();
+                    Set<String> landlord_properties = default_properties.fetchAllproperty(user_id.landlordId(email));
+
+
+                    model1.put("names",landlord_properties);
+                    model2.put("unit",clone_unit);
+                    data.putAll(model);
+                    data.putAll(model1);
+                    data.putAll(model2);
+                    data.putAll(MODEL);
+
+                    System.out.println("backup_dashboard");
+                    System.out.println(data);
+                    ctx.render("templates/dashboard.html",data);
+            }
+                {
+                }
             } catch (SQLException e) {
                 if("paid".equals(e.getMessage())){
                     ctx.sessionAttribute("error",e.getMessage());
