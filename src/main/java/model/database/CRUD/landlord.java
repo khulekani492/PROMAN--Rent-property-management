496f3b3fbf00f4c0af6d3210e0ce4b4b1c529555
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-17 20:52:09 +0200

    Added additional methods property_name, user_name by user_id returns values for the PersonEmail to create dynamic content for each email catered for the landlord who has units due

--- src/main/java/model/database/CRUD/landlord.java
+++ src/main/java/model/database/CRUD/landlord.java
@@ -5,6 +5,7 @@ import model.database.ConnectionAccess;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class landlord extends ConnectionAccess {
 
@@ -27,9 +28,59 @@ public class landlord extends ConnectionAccess {
         }
         return id;
     }
+        public String landlord_username(Integer userID){
+        String sql = """
+                SELECT name FROM general_users WHERE id=?
+                limit 1""";
+
+         String username = "";
+        try {
+            PreparedStatement pstm = this.connection.prepareStatement(sql);{
+                pstm.setInt(1,userID);
+                ResultSet result = pstm.executeQuery();
+                if(result.next()){
+                   username = result.getString("name");
+
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return username;
+    }
+
+
+    public String landlord_property_name(Integer landlordId) {
+
+        String sql = """
+                SELECT property_name FROM properties WHERE id=?
+                limit 1""";
+
+        String username = "";
+        try {
+            PreparedStatement pstm = this.connection.prepareStatement(sql);{
+                pstm.setInt(1,landlordId);
+                ResultSet result = pstm.executeQuery();
+                if(result.next()){
+                    username = result.getString("property_name");
+
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return username;
+    }
+
     public void main(String[] args){
         landlord baby_wait = new landlord();
-        System.out.println(baby_wait.landlordId("khulekaniszondo6@gmail.com") );
 
+        System.out.println(baby_wait.landlordId("khulekaniszondo6@gmail.com") );
+        System.out.println(baby_wait.landlord_username(771));
     }
+
+
+//    public String landlord_units(ArrayList<String> due_units) {
+//
+//    }
 }
