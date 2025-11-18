6aacdc3b4e2f7158daf616c9ff6371d4aa9cb2d3
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-18 12:22:19 +0200

    removed singleton connection

--- src/main/java/schedule/Quartz.java
+++ src/main/java/schedule/Quartz.java
@@ -1,10 +1,10 @@
 package schedule;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.Trigger;
+import org.quartz.*;
 import org.quartz.impl.StdScheduler;
 import org.quartz.impl.StdSchedulerFactory;
+
+import static org.quartz.CronScheduleBuilder.cronSchedule;
+import static org.quartz.CronScheduleBuilder.dailyAtHourAndMinute;
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
@@ -17,16 +17,24 @@ public class Quartz {
         JobDetail test_job = newJob(Reminder.class)
                 .withIdentity("emails","group1")
                 .build();
+        JobDetail test_job2 = newJob(Reminder.class)
+                .withIdentity("second_reminder","group1")
+                .build();
 
         Trigger trigger = newTrigger().
                 withIdentity("my_trigger","group1")
-                .startNow()
-                .withSchedule(simpleSchedule()
-                        .withIntervalInSeconds(40)
-                        .repeatForever())
+                .startNow().withSchedule(dailyAtHourAndMinute(12, 5))
                 .build();
 
+                  Trigger trigger2 = newTrigger().
+                withIdentity("second_trigger","group2")
+                .startNow().withSchedule(dailyAtHourAndMinute(12,10))
+                .build();
+
+
         scheduler.scheduleJob(test_job,trigger);
+        scheduler.scheduleJob(test_job2,trigger2);
+
 
 
     }
