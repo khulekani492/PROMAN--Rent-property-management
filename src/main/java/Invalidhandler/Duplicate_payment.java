ccb68edbec03eb96ba16d6e6a463f341bdbeeb8b
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-20 21:01:49 +0200

    fetches the property_name from session

--- src/main/java/Invalidhandler/Duplicate_payment.java
+++ src/main/java/Invalidhandler/Duplicate_payment.java
@@ -9,9 +9,9 @@ public class Duplicate_payment implements ErrorHandler {
     @Override
     public Handler error_message() {
         return ctx ->{
-            String before_state = ctx.sessionAttribute("dashBoard_current_property");
+            String before_state = ctx.sessionAttribute("property_name");
             System.out.println("Before state " + before_state);
-            String before_state_email = ctx.sessionAttribute("dashBoard_current_email");
+            String before_state_email = ctx.sessionAttribute("email");
             String unit = ctx.sessionAttribute("unit");
             System.out.println("Before state email " + before_state_email);
             ctx.redirect("/unit_already_paid/" + before_state +"/"+before_state_email +"/" +unit);
