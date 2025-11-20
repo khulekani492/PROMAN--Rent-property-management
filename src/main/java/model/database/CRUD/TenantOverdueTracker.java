53a52dd2639122dbd99d9cec6300978fb3c1fc24
khzondojhb024 <khzondojhb024@student.wethinkcode.co.za>
2025-11-20 09:15:36 +0200

    calls postgres function to update overdue_date of tenants who are pass they rent date

--- src/main/java/model/database/CRUD/TenantOverdueTracker.java
+++ src/main/java/model/database/CRUD/TenantOverdueTracker.java
@@ -1,6 +1,7 @@
 package model.database.CRUD;
 
 import model.database.ConnectionAccess;
+import org.postgresql.shaded.com.ongres.scram.common.bouncycastle.pbkdf2.Strings;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -9,22 +10,27 @@ public class TenantOverdueTracker extends ConnectionAccess {
   private  Integer landlordId;
 
 
-  public TenantOverdueTracker(Integer landlordId){
+  public TenantOverdueTracker(){
       this.landlordId = landlordId;
 
   }
-  public void call_postgres_func() throws SQLException {
-     String sql = """
-              SELECT update_overdue_date_by_landlord(?);
-              """ ;
-      try(PreparedStatement pstmt = this.connection.prepareStatement(sql)){
-                 pstmt.setInt(1,this.landlordId);
-                 pstmt.executeUpdate();
+    public void call_postgres_func(Integer landlordId) {
+        String sql = "SELECT update_overdue_date_by_landlord(?)";
 
-      } catch (SQLException e){
-          System.out.println(e.getMessage());
-          throw new RuntimeException("ayi update");
-      }
+        try (PreparedStatement pstmt = this.connection.prepareStatement(sql)) {
+
+            pstmt.setInt(1, landlordId);
+            pstmt.execute();
+
+        } catch (SQLException e) {
+            System.out.println("SQL Error: " + e.getMessage());
+            throw new RuntimeException("Function failed");
+        }
+    }
+
+public static  void main(Strings[] args) throws SQLException {
+      Integer sa = 771;
+      TenantOverdueTracker new_day = new TenantOverdueTracker();
+      new_day.call_postgres_func(sa);
   }
-
 }
