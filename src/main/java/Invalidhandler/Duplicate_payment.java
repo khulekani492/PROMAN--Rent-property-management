9cfc206d160a98d3ba71a32816e673d9d9980c0b
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-21 10:59:04 +0200

    get property_name,email,unit from session,redirect to

--- src/main/java/Invalidhandler/Duplicate_payment.java
+++ src/main/java/Invalidhandler/Duplicate_payment.java
@@ -9,13 +9,11 @@ public class Duplicate_payment implements ErrorHandler {
     @Override
     public Handler error_message() {
         return ctx ->{
-            String before_state = ctx.sessionAttribute("property_name");
-            System.out.println("Before state " + before_state);
+            String before_state_property = ctx.sessionAttribute("propertyName");
             String before_state_email = ctx.sessionAttribute("email");
             String unit = ctx.sessionAttribute("unit");
             System.out.println("Before state email " + before_state_email);
-            ctx.redirect("/unit_already_paid/" + before_state +"/"+before_state_email +"/" +unit);
-
+            ctx.redirect("/unit_already_paid/" + before_state_property +"/"+before_state_email +"/" +unit);
 
         } ;
     }
