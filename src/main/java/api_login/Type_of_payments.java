72b07ff9a81176a7ffee070d2f5c68d68cca0a8a
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-22 10:47:24 +0200

    Type of payments handled with backgrounder worker that set debt according to the received money

--- src/main/java/api_login/Type_of_payments.java
+++ src/main/java/api_login/Type_of_payments.java
@@ -28,20 +28,18 @@ public class Type_of_payments {
                 approve_to_rentBook.setStatus(true);
                 approve_to_rentBook.setTenant_Id(tenantId);
                 approve_to_rentBook.update_tenant_status();
-                return ;
-                //else if the tenant existing debt ia greater than money recieved
-                //minus debt with the recieved money
-            } else if ( debt > recieved_amount) {
+
+            }
+            //else if the tenant existing debt ia greater than money recieved
+            else if ( debt > recieved_amount) {
                 Integer money_paid_left = debt - recieved_amount;
                 System.out.println("Deducts ;" + money_paid_left);
-                new Tenant().update_debt(money_paid_left,tenantId);
-                approve_to_rentBook.setAmount_paid(money_paid_left);
+                new Tenant().update_debt(money_paid_left,tenantId); //update tenant to the remaining debt
+                approve_to_rentBook.setAmount_paid(original_rent);
                 approve_to_rentBook.setStatus(true);
                 approve_to_rentBook.setTenant_Id(tenantId);
                 approve_to_rentBook.update_tenant_status();
-                //Check the money_paid_left
-                //update properties.rent_book
-                //
+
             } else if (recieved_amount > debt) {
                 Integer money_paid_left = recieved_amount - debt;
                 System.out.println("Settles debt : " + money_paid_left);
@@ -50,22 +48,37 @@ public class Type_of_payments {
                 approve_to_rentBook.setStatus(true);
                 approve_to_rentBook.setTenant_Id(tenantId);
                 approve_to_rentBook.update_tenant_status();
-                //Check if money left after settling the debt is greater than original_rent --> calculate advance_pay
+                //Check if money left after settling the debt is greater than original_rent --> update tenant.debt
                 if (money_paid_left < original_rent){
                     new Tenant().update_debt(money_paid_left,tenantId);
                 }
             }
             else {
                 //When payment_paid equals to debt owed by tenant ,settle debt of the previous, updates the tenant debt of the new month
-                Integer new_amount = original_rent; // R400
-                new Tenant().update_debt(new_amount,tenantId); //update to tenant debt
+//                The amount received is exactly equal to the outstanding debt.
+//                This means the tenant pays precisely the amount they owe, no more, no less.
+                new Tenant().update_debt(original_rent,tenantId); //update to tenant debt
                 approve_to_rentBook.setAmount_paid(recieved_amount);
                 approve_to_rentBook.setStatus(true);
                 approve_to_rentBook.setTenant_Id(tenantId);
                 approve_to_rentBook.update_tenant_status();
-                return ;
+
             }
-            return ;
+
+        }
+        //Advance_payment_block add tenantId and period the advance payment covers
+        else if (recieved_amount > original_rent) {
+            Record_advance_payment sharedMap_advance_pays = new Record_advance_payment();
+            HashMap<Integer,Integer> add_to_Map = sharedMap_advance_pays.current_property_advancePayments;
+            Integer period = recieved_amount / original_rent;
+            add_to_Map.put(tenantId,period);
+
+        }
+        // if money received is lesser than original_rent , record the outstanding fee
+        else {
+            Integer outstanding_fee = original_rent - recieved_amount;
+            Integer  add_debt = debt + outstanding_fee;
+            new Tenant().update_debt(add_debt,tenantId);
         }
 
     }
