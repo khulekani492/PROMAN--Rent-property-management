fdb986cc6c6d68492c152b46e4465ec32581da25
khzondojhb024 <khzondojhb024@student.wethinkcode.co.za>
2025-11-29 17:14:00 +0200

    to be updated

--- src/main/java/API_handlers/AddTenant.java
+++ src/main/java/API_handlers/AddTenant.java
@@ -13,7 +13,26 @@ import java.util.Map;
 public class AddTenant {
     public Handler addTenant() {
         return ctx -> {
+            //access property name and address
+            String property_name = ctx.sessionAttribute("property_name");
+            String property_address =  ctx.sessionAttribute("property_address");
+            Integer chosen_unit = Integer.valueOf(ctx.formParam("unit_number"));
+            Integer landlordId = ctx.sessionAttribute("user_ID");
+            residence property_information = new residence();
+            property_information.setProperty_Name(property_name);
+            property_information.setProperty_address(property_address);
+            property_information.setProperty_unit(chosen_unit);
+            property_information.setlandlord(landlordId);
+
             try {
+                try {
+                    property_information.insert_information();
+                } catch (Exception e) {
+                    ctx.sessionAttribute("error", "Unit already taken");
+                    ctx.redirect("/error/same_unit");
+                    return;
+                }
+
                 // Insert tenant into general_user table for landlord manual insertion
                 String name = ctx.formParam("tenant_name");
                 String number = ctx.formParam("cell_number");
@@ -36,28 +55,17 @@ public class AddTenant {
                 System.out.println("Tenant ID: " + tenantUniqueID);
 
                 // Update properties table with tenant unique ID
-                Integer landlordId = ctx.sessionAttribute("user_ID");
-                Integer property_unit = ctx.sessionAttribute("property_unit");
-                String property_Name = ctx.sessionAttribute("property_name");
                 String property = ctx.sessionAttribute("residence");
-                System.out.println("Property_name from cookie session: " + property_Name );
                 System.out.println("Property_residence: " + property );
                 System.out.println("Landlord ID: " + landlordId);
-                System.out.println("Property unit: " + property_unit);
 
-                residence addTenantUnit = new residence();
-                // create row first Insert information
-
-
-                addTenantUnit.setProperty_unit(property_unit);
-                addTenantUnit.setProperty_Name(property_Name);
-                addTenantUnit.setlandlord(landlordId);
-                addTenantUnit.setTenantId(tenantUniqueID);
+                property_information.setTenantId(tenantUniqueID);
                 System.out.println("Rent Payment ");
-                addTenantUnit.setRent(rent_pay);
+                property_information.setRent(rent_pay);
+
 
                 try {
-                    addTenantUnit.Insert_tenatId();
+                    property_information.Insert_tenatId();
 
                 } catch (SQLException e) {
                     ctx.redirect("/user_sign_up/error");
@@ -84,14 +92,12 @@ public class AddTenant {
 
 
                 // Render dashboard with updated model
-                String username = ctx.sessionAttribute("name");
-                String property_name = ctx.sessionAttribute("property_Name");
-                Integer unit_number = ctx.sessionAttribute("_unit");
+//                String username = ctx.sessionAttribute("name");
+//                //String property_name = ctx.sessionAttribute("property_Name");
+//                Integer unit_number = ctx.sessionAttribute("_unit");
 
-                Map<String, Object> model = new HashMap<>();
-                model.put("name", property_name);
-
-
+//                Map<String, Object> model = new HashMap<>();
+//                model.put("name", property_name);
                 ctx.redirect("/dashboard");
                 //ctx.render("/templates/dashboard.html", model);
 
