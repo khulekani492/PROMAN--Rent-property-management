8be81ad7deb58903b0a6464c5c9b6b4d21666ae5
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-11-30 12:39:19 +0200

    thymleaf values in case a user  submit unit already submitted

--- src/main/java/Invalidhandler/SameUnit.java
+++ src/main/java/Invalidhandler/SameUnit.java
@@ -9,17 +9,34 @@ public class SameUnit implements  ErrorHandler{
     @Override
     public Handler error_message() {
         return ctx -> {
-
-            String  update_user = ctx.sessionAttribute("error");
             Map<String, Object> model = new HashMap<>();
             String user_name = ctx.sessionAttribute("user_name");
+
+            String tenant_name = ctx.sessionAttribute("tenant_name");
+            String tenant_number = ctx.sessionAttribute("tenant_number");
+            String tenant_pay = ctx.sessionAttribute("tenant_pay");
+            String tenant_moveIn = ctx.sessionAttribute("tenant_MoveIn");
+            String tenant_kin_number = ctx.sessionAttribute("tenant_kin_number");
+            String tenant_kin_name = ctx.sessionAttribute("tenant_kin_number");
+            String rent_payment_day = ctx.sessionAttribute("tenant_rent_payment");
+            Integer tenant_debt = ctx.sessionAttribute("debt");
+
+
             String  propertyName = ctx.sessionAttribute("property_name");
             Integer  propertyUnit = ctx.sessionAttribute("property_unit");
 
+            model.put("tenant_name",tenant_name);
+            model.put("tenant_pay",tenant_pay);
+            model.put("tenant_number",tenant_number);
+            model.put("tenant_moveIn",tenant_moveIn);
+            model.put("tenant_kin_number",tenant_kin_number);
+            model.put("tenant_kin_name",tenant_kin_name);
+            model.put("tenant_rent_day",rent_payment_day);
+            model.put("tenant_debt",tenant_debt);
 
-            model.put("user_name",user_name);
             model.put("unit_add", propertyUnit);
             model.put("name", propertyName);
+
             model.put("error","already taken");
             System.out.println(model);
             ctx.render("templates/tenant_form.html",model);
