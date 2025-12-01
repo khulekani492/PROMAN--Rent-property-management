b5ccfe15d48852da4acc0fd106786f0b4cc369f2
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-12-02 01:07:17 +0200

    Insert information updates the existing row with tenant

--- src/main/java/API_handlers/AddTenant.java
+++ src/main/java/API_handlers/AddTenant.java
@@ -16,24 +16,12 @@ public class AddTenant {
             //access property name and address
             String property_name = ctx.sessionAttribute("property_name");
             String property_address =  ctx.sessionAttribute("property_address");
+
             Integer chosen_unit = Integer.valueOf(ctx.formParam("unit_number"));
-            Integer landlordId = ctx.sessionAttribute("user_ID");
+            ctx.sessionAttribute("occupied_unit_error",chosen_unit);
             residence property_information = new residence();
-            property_information.setProperty_Name(property_name);
-            property_information.setProperty_address(property_address);
-            property_information.setProperty_unit(chosen_unit);
-            property_information.setlandlord(landlordId);
 
             try {
-                try {
-                    property_information.insert_information();
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                    ctx.sessionAttribute("error", "Unit already taken");
-                    ctx.redirect("/error/same_unit");
-                    return;
-                }
-
                 // Insert tenant into general_user table for landlord manual insertion
                 String name = ctx.formParam("tenant_name");
                 ctx.sessionAttribute("tenant_name",name);
@@ -42,8 +30,6 @@ public class AddTenant {
                 String rent_pay = ctx.formParam("rent_amount");
                 ctx.sessionAttribute("tenant_pay",rent_pay);
 
-
-                System.out.println("Imali yami " + rent_pay);
                 String user_type = "tenant";
 
                 general add_newTenant = new general(name, number, user_type);
@@ -52,21 +38,15 @@ public class AddTenant {
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
-
                 // Access the unique ID from the general_user table
                 Integer tenantUniqueID = add_newTenant.tenant_ID();
-                System.out.println("Tenant ID: " + tenantUniqueID);
-
                 // Update properties table with tenant unique ID
-                System.out.println("Landlord ID: " + landlordId);
-
                 property_information.setTenantId(tenantUniqueID);
-
                 property_information.setRent(rent_pay);
-                System.out.println(rent_pay +" rent amount");
-
+                property_information.setProperty_unit(chosen_unit);
+                property_information.setProperty_Name(property_name);
                 try {
-                    property_information.Insert_tenatId();
+                    property_information.insert_information();
 
                 } catch (SQLException e) {
                     ctx.redirect("/user_sign_up/error");
@@ -75,9 +55,7 @@ public class AddTenant {
 
                 }
 
-
                 // Additional tenant info
-
                 Date moveIn = Date.valueOf(ctx.formParam("move_in"));
                 ctx.sessionAttribute("tenant_moveIn",moveIn);
                 String employment_status = ctx.formParam("employment");
@@ -104,15 +82,6 @@ public class AddTenant {
                     throw new RuntimeException(e);
                 }
 
-
-                // Render dashboard with updated model
-//                String username = ctx.sessionAttribute("name");
-//                //String property_name = ctx.sessionAttribute("property_Name");
-//                Integer unit_number = ctx.sessionAttribute("_unit");
-
-//                Map<String, Object> model = new HashMap<>();
-//                model.put("name", property_name);
-             //   ctx.redirect("/dashboard");
                 ctx.redirect("/dashboard");
 
             } catch (SQLException e) {
