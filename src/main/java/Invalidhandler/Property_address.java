7e88b5a43d19edfac4d56c822f2507a76ec7fee3
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-12-03 02:52:45 +0200

    factory method and  url_endpoint for same property

--- src/main/java/Invalidhandler/Property_address.java
+++ src/main/java/Invalidhandler/Property_address.java
@@ -1,4 +1,19 @@
 package Invalidhandler;
 
-public class Property_address {
+import io.javalin.http.Handler;
+
+import java.util.HashMap;
+
+public class Property_address implements  ErrorHandler{
+    @Override
+    public Handler error_message() {
+        return ctx ->{
+            String property_name = ctx.sessionAttribute("property_name");
+            String property_address = ctx.sessionAttribute("property_address");
+            HashMap<String,String> model = new HashMap<>();
+            model.put("property_error",  property_name  );
+            model.put("property_name",property_address);
+            ctx.render("templates/property.html",model);
+        };
+    }
 }
