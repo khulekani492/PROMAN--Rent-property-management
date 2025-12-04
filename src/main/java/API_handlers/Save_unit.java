3052242bf454417bfc7dc647d8db0e6f6a433ec7
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-12-04 17:30:41 +0200

    dark theme effect

--- src/main/java/API_handlers/Save_unit.java
+++ src/main/java/API_handlers/Save_unit.java
@@ -11,11 +11,14 @@ public class Save_unit {
             String getUsername = ctx.sessionAttribute("user_name");
             String propertyName = ctx.sessionAttribute("property_name");
             String propertyAddress = ctx.sessionAttribute("property_address");
+            String themeColor = ctx.sessionAttribute("theme_color");
+
             System.out.println(propertyAddress + " played the game");
             HashMap<String,String> model = new HashMap<>();
             model.put("user_name",getUsername);
             model.put("property_name",propertyName);
             model.put("property_address",propertyAddress);
+            model.put("user_chosen_theme",themeColor);
             ctx.render("templates/property.html",model);
         };
     }
