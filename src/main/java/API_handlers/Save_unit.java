df6cbd9e78a17a3ff1754a5edb8f8aeaac4e32ca
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-12-05 07:32:17 +0200

    Refactored --removed redudant memory acess

--- src/main/java/API_handlers/Save_unit.java
+++ src/main/java/API_handlers/Save_unit.java
@@ -8,16 +8,12 @@ public class Save_unit {
 
     public Handler save_property_unit(){
         return  ctx -> {
-            String getUsername = ctx.sessionAttribute("user_name");
-            String propertyName = ctx.sessionAttribute("property_name");
-            String propertyAddress = ctx.sessionAttribute("property_address");
-            String themeColor = ctx.sessionAttribute("theme_color");
 
-            System.out.println(propertyAddress + " played the game");
+
+            String getUsername = ctx.sessionAttribute("user_name");
+            String themeColor = ctx.sessionAttribute("theme_color");
             HashMap<String,String> model = new HashMap<>();
             model.put("user_name",getUsername);
-            model.put("property_name",propertyName);
-            model.put("property_address",propertyAddress);
             model.put("user_chosen_theme",themeColor);
             ctx.render("templates/property.html",model);
         };
