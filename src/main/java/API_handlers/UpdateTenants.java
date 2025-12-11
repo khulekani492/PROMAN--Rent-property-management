4b8770df9f500cac5450549b95da8361732b7b05
khzondojhb024 <khzondojhb024@student.wethinkcode.co.za>
2025-12-11 17:29:09 +0200

    contact unique id for tenant ID

--- src/main/java/API_handlers/UpdateTenants.java
+++ src/main/java/API_handlers/UpdateTenants.java
@@ -13,14 +13,16 @@ public class UpdateTenants {
     public Handler unmark_payment(){
         return ctx ->{
             // --- 1. Data Extraction ---
+            System.out.println("Unmarking the payment");
             Tenant undo_tenant_payment = new Tenant();
 
             // Getting IDs and Property Info (Necessary for payment logic and subsequent page reload if needed)
             String tenant_name = ctx.formParam("id");
+            String tenant_contact = ctx.pathParam("tenantContact");
             ctx.sessionAttribute("ID",tenant_name);
-            String contact = ctx.sessionAttribute("tenant_contact");
+            //String contact = ctx.sessionAttribute("tenant_contact");
             //Integer
-            Integer tenantId = undo_tenant_payment.tenant_ID(tenant_name,contact);
+            Integer tenantId = undo_tenant_payment.tenant_ID(tenant_name,tenant_contact);
             
             //removes payment from rent_book 
             undo_tenant_payment.unmark_tenant_rent(tenantId);
