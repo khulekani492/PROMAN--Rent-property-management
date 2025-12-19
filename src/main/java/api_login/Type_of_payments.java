41c5734cccbeb32a84a87703aee249e9e33c0ae9
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-12-19 07:47:09 +0200

    Type of payments calculation logic

--- src/main/java/api_login/Type_of_payments.java
+++ src/main/java/api_login/Type_of_payments.java
@@ -46,38 +46,41 @@ public class Type_of_payments {
                 approve_to_rentBook.setDebtString(String.valueOf(remaining_debt));
 
             } else {
-                Integer remaining_debt =recieved_amount -  debt ; //Minus the debt with the money received
-                System.out.println("Debt exists and is lesser than the rent money paid , after deducting debt " + remaining_debt);
-                System.out.println()
-                ;
+                Integer remaining_amount = recieved_amount -  debt ; //Minus the debt with the money received
+                System.out.println("Debt exists and is lesser than the rent money paid , after deducting the debt " + remaining_amount);
+                System.out.println();
                 System.out.println("Received MONEY :   " + recieved_amount);
                 System.out.println("Tenant Debt :   " + debt);
-                System.out.println("new debt " + remaining_debt);
-                new Tenant().update_debt(remaining_debt, tenantId); //update tenant to the remaining debt
-                approve_to_rentBook.setAmount_paid(original_rent);
-                approve_to_rentBook.setStatus(true);
-                approve_to_rentBook.setTenant_Id(tenantId);
-                approve_to_rentBook.update_tenant_status();
-                approve_to_rentBook.setDebtString(String.valueOf(remaining_debt));
+                System.out.println("Remaining amount after payment :  " + remaining_amount);
+
+                if (remaining_amount.equals(original_rent)){
+                    System.out.println("Tenant has no debt after settling ");
+                    approve_to_rentBook.setAmount_paid(recieved_amount);
+                    approve_to_rentBook.setStatus(true);
+                    approve_to_rentBook.setTenant_Id(tenantId);
+                    approve_to_rentBook.update_tenant_status();
+                    approve_to_rentBook.setDebtString("0");
+                } else if(remaining_amount > original_rent ){
+                    approve_to_rentBook.setAmount_paid(recieved_amount);
+                    approve_to_rentBook.setStatus(true);
+                    approve_to_rentBook.setTenant_Id(tenantId);
+                    approve_to_rentBook.update_tenant_status();
+
+                    // Advance_payment period
+                    Integer period = recieved_amount  % original_rent;
+                    approve_to_rentBook.setDebtString("Advance_payment + " + period );
+                } else {
+                    Integer new_debt = original_rent - remaining_amount;
+                    approve_to_rentBook.setAmount_paid(recieved_amount);
+                    approve_to_rentBook.setStatus(true);
+                    approve_to_rentBook.setTenant_Id(tenantId);
+                    approve_to_rentBook.update_tenant_status();
+                    approve_to_rentBook.setDebtString(String.valueOf(  new_debt));
+                    System.out.println("paid less money than original rent");
+
+                }
 
             }
-                //Advance_payment_block add tenantId and period the advance payment covers
-
-                //else if the tenant existing debt ia greater than money recieved
-
-
-                // if money received is lesser than original_rent , record the outstanding fee
-
-
-
-
-//            void main () throws SQLException {
-//                Type_of_payments update_debt = new Type_of_payments();
-//                Tenant tenantTest = new Tenant();
-//                // tenantTest.
-//                update_debt.type_of_payment(400, 400, tenantTest.tenant_debt(772), 772, new Transaction());
-//                //Output debt = 800
-//            }
 
         } else if (recieved_amount > original_rent) {
             System.out.println("recieved_amount is greater than original_rent " + recieved_amount);
@@ -91,8 +94,6 @@ public class Type_of_payments {
                 // Advance_payment period
                 Integer period = recieved_amount  % original_rent;
                 approve_to_rentBook.setDebtString("Advance_payment + " + period );
-
-
             }
             //else if the tenant existing debt ia greater than money recieved
             else if (debt > recieved_amount) {
@@ -153,14 +154,12 @@ public class Type_of_payments {
 
 
         } else {
-
             Integer new_debt = original_rent - recieved_amount;
             approve_to_rentBook.setAmount_paid(recieved_amount);
             approve_to_rentBook.setStatus(true);
             approve_to_rentBook.setTenant_Id(tenantId);
             approve_to_rentBook.update_tenant_status();
             approve_to_rentBook.setDebtString(String.valueOf(  new_debt));
-
             System.out.println("paid less money than original rent");
         }
 
