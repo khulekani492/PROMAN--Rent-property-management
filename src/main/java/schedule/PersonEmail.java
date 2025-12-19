6d4438b84c5cb4c0d2b0b5a44660303eb2cd51a6
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-12-19 15:51:27 +0200

    SMSBUlk set up

--- src/main/java/schedule/PersonEmail.java
+++ src/main/java/schedule/PersonEmail.java
@@ -2,14 +2,22 @@ package schedule;
 
 import model.database.CRUD.landlord;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-
+import java.util.Base64;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 public class PersonEmail {
     private Integer landlordId;
     private ArrayList<Integer> tenants_due;
 
 
-    public PersonEmail(Integer landlordId,ArrayList<Integer> tenants_due){
+    public PersonEmail(){
         this.landlordId = landlordId;
         this.tenants_due = tenants_due;
     }
@@ -51,5 +59,116 @@ public String send_email(){
 
     return email.toString();
 }
+//public String sendSMS(String property_owner_name, String property_contact, ArrayList tenants_due){
+//        String jsonBuilder = """
+//                {
+//                "body" : "proMan Reminder, Hi {F0##}, log in to mark units {F1################} who have paid",
+//                 "to":  [
+//                      {"address": ${property},"fields": [${property_contact},${tenants_due}] }
+//                  ]
+//                }
+//                """;
+//        return jsonBuilder;
+//}
+
+//    public String sendSMS(String propertyOwnerName,
+//                          String propertyContact,
+//                          ArrayList<String> tenantsDue) {
+//
+//        // Convert tenant list to readable text
+//        String tenantsDueText = tenantsDue.stream()
+//                .collect(Collectors.joining(", "));
+//
+//        String jsonBody = """
+//        {
+//          "messages": [
+//            {
+//              "body": "proMan Reminder, Hi {F0#}, log in to mark units {F1} who have paid",
+//              "to": [
+//                {
+//                  "address": "%s",
+//                  "fields": ["%s", "%s"]
+//                }
+//              ]
+//            }
+//          ]
+//        }
+//        """.formatted(
+//                propertyContact,
+//                propertyOwnerName,
+//                tenantsDueText
+//        );
+//
+//        return jsonBody;
+//    }
+
+    public String sendSMS(String propertyOwnerName,
+                          String propertyContact,
+                          ArrayList<String> tenantsDue) {
+
+        String tenantsDueText = String.join(", ", tenantsDue);
+
+        return """
+    [
+      {
+        "body": "proMan Reminder, Hi {F0########}, log in to mark units {F1} who have paid",
+        "to": [
+          {
+            "address": "%s",
+            "fields": ["%s", "%s"]
+          }
+        ]
+      }
+    ]
+    """.formatted(
+                propertyContact,
+                propertyOwnerName,
+                tenantsDueText
+        );
+    }
+
+
+    public HttpRequest makeRequest(String jsonBody) {
+
+        String tokenId = "54B80C520D704654A5431C8DA1D9CA8D-01-B";       // username
+        String tokenSecret = "vmPZOUxRA#Ymw8yLNx29OZD6v_rM6"; // apiKey
+        String apiUrl = "https://api.bulksms.com/v1/messages";
+
+        String auth = tokenId + ":" + tokenSecret;
+        String encodedAuth = Base64.getEncoder()
+                .encodeToString(auth.getBytes(StandardCharsets.UTF_8));
+
+        return HttpRequest.newBuilder()
+                .uri(URI.create(apiUrl))
+                .header("Authorization", "Basic " + encodedAuth)
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                .build();
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        ArrayList<String> tenants = new ArrayList<>();
+        tenants.add("A1");
+        tenants.add("B5");
+
+        PersonEmail sms = new PersonEmail();
+
+        String json = sms.sendSMS(
+                "Khulekani",
+                "+27826690384",
+                tenants
+        );
+
+        HttpRequest request = sms.makeRequest(json);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Status: " + response.statusCode());
+        System.out.println("Response: " + response.body());
+    }
 
 }
