fac7dee518fc900a00aaf73c9085f0574b7eda4e
khulekaniszondo6@gmail.com <khulekaniszondo6@gmail.com>
2026-02-18 21:10:21 +0200

    Cloud Database restarted

--- src/main/java/model/main.java
+++ src/main/java/model/main.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 public class main {
 
     public static void main(String[] args) {
-        //            residence newoens = new  residence("Sunset Villas",20,4,5000,"lase","03283833",1);
+        //            residence newoens = new  residence("Sunset Villas",20,4,5000,"lase","03283833",1); Old NOTES
 //            newoens.insert_information();
 //            Tenant newss = new Tenant(1,  "KILR",  Date.valueOf("2025-10-06"),   "yes",  "0823343233",  Date.valueOf("2020-10-06"), 5, 4500,  "d","09883888394"
 //            );
